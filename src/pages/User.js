8ba96f9a68fd44701ea626db69d789ccb6a0dd7e
abdera2004 <ruanpereira405@gmail.com>
2024-04-07 18:52:20 -0300

    tela de cadastro arrumada

--- src/pages/User.js
+++ src/pages/User.js
@@ -1,6 +1,6 @@
 //Cadastro do usuário
 
-import { StyleSheet, Text, View, TextInput, ImageBackground, Pressable, Alert } from 'react-native';
+import { StyleSheet, Text, View, TextInput, ImageBackground, Pressable, Alert, Image } from 'react-native';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import * as Animado from 'react-native-animatable';
 import AsyncStorage from '@react-native-async-storage/async-storage';
@@ -77,27 +77,46 @@ export default function User({navigation}) {
   return (
     <View style={styles.container}>
 
-      <ImageBackground source={require('../../assets/fundo-app-zoo-kids.png')} style={styles.fundo}>
+      <ImageBackground source={require('../../assets/fundo/fundo-cadastro.png')} style={styles.fundo}>
       
         <Animado.View animation="fadeInLeft" duration={2000} style={styles.formulario}>
 
-          <View style={styles.textoView}>
-            <Text style={styles.cadastro}>CADASTRO</Text>
-          </View>
+          <ImageBackground source={require('../../assets/fundo/fundo-formulario-cadastro.png')} style={styles.fundoFormulario}>
 
-          <TextInput style={styles.input} onChangeText={setNome} value={nome} id='nome' autoComplete='name' textContentType='name' placeholder='Digite o seu nome...' />
+            <View style={styles.textoView}>
+              <Text style={styles.cadastro}>CADASTRO</Text>
+            </View>
 
-          <TextInput style={styles.input} onChangeText={setIdade} value={idade} id='idade' placeholder='Digite sua idade...' />
+            <View style={styles.section}>
+              <FontAwesome5 name="user-circle" size={25} color="black" style={styles.icone}/>
+              <TextInput style={styles.input} onChangeText={setNome} value={nome} id='nome' autoComplete='name' textContentType='name' placeholder='Digite o seu nome...' />
+            </View>
 
-          <TextInput style={styles.input} onChangeText={setEmail} value={email} id='email' autoComplete='email' textContentType='emailAddress' placeholder='Digite o seu e-mail...' />
+            <View style={styles.section}>
+              <FontAwesome5 name="birthday-cake" size={25} color="black" style={styles.icone}/>
+              <TextInput style={styles.input} onChangeText={setIdade} value={idade} id='idade' placeholder='Digite sua idade...' />
+            </View>
 
-          <TextInput style={styles.input} onChangeText={setSenha} value={senha} id='senha' autoComplete='password' textContentType='password' placeholder='Digite sua senha...' />
+            <View style={styles.section}>
+              <FontAwesome5 name="envelope" size={25} color="black" style={styles.icone}/>
+            <TextInput style={styles.input} onChangeText={setEmail} value={email} id='email' autoComplete='email' textContentType='emailAddress' placeholder='Digite o seu e-mail...' />
+            </View>
 
-          <View style={styles.textoEInput2}>
-            <Pressable style={styles.botao} onPress={onPressButton}>
-              <Text style={styles.textoFormulario}>CADASTRAR</Text>
-            </Pressable>
-          </View>
+            <View style={styles.section}>
+              <FontAwesome5 name="lock" size={25} color="black" style={styles.icone}/>
+            <TextInput style={styles.input} onChangeText={setSenha} value={senha} id='senha' autoComplete='password' textContentType='password' placeholder='Digite sua senha...' />
+            </View>
+
+            <View style={styles.textoEInput2}>
+              <Pressable onPress={onPressButton}>
+                <Image
+                  source={require('../../assets/botoes/placa cadastrar.png')}
+                  style={styles.botao}
+                />
+              </Pressable>
+            </View>
+
+          </ImageBackground>
 
         </Animado.View>
 
@@ -115,8 +134,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   formulario: {
-    flex: 0.8,
-    width: '80%',
+    flex: 0.6,
+    width: 220,
     flexDirection: 'column',
     borderWidth: 2,
     borderRadius: 15,
@@ -124,28 +143,19 @@ const styles = StyleSheet.create({
     backgroundColor: '#f8cd23',
     alignItems: 'center',
     justifyContent:'space-evenly',
-    paddingHorizontal: 20
+  },
+  fundoFormulario: {
+    resizeMode: 'cover',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    flex: 1,
+    overflow: 'hidden',
   },
   input: {
-    borderWidth: 1,
-    borderRadius: 50,
-    height: 50,
-    width: '95%',
-    textAlign: 'center',
-    borderColor: '#604502',
-    fontFamily: 'Comic Sans MS'
-  },
-  textoFormulario: {
-    color: 'white',
-    fontWeight: 'bold',
-    alignItems: 'center',
-    justifyContent: 'center',
-    fontSize: 20,
-    fontFamily: 'Comic Sans MS'
-  },
-  texto: {  
-    color: 'white',
-    fontWeight: 'bold'
+    flex: 1,
+    fontFamily: 'Comic Sans MS',
+    fontSize: 10,
+    height: '100%'
   },
   fundo: {
     width: '100%',
@@ -155,57 +165,44 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     flex: 1,
   },
-  fundoBotao: {
-    width: '100%',
-    height: '100%',
-    justifyContent: 'center',
-    alignItems: 'center',
-    resizeMode: 'contain',
-    flex: 1,
-  },
-  textoEInput: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    width: '100%',
-    flexDirection: 'row',
-  },
   textoEInput2: {
     alignItems: 'center',
     width: '100%'
   },
   botao: {
     width: 200,
-    height: 80,
+    height: 90,
     alignItems: 'center',
     justifyContent: 'center',
-    borderRadius: 50,
     overflow: 'hidden',
-    backgroundColor: '#11390F',
-
+    flex: 0.25
   },
   cadastro: {
     fontWeight: 'bold',
     color: 'black',
-    fontSize: 40,
+    fontSize: 30,
     borderBottomWidth: 2,
-    fontFamily: 'Comic Sans MS'
+    fontFamily: 'Comic Sans MS',
+    marginStart: 10
   },
   textoView: {
     width: '100%',
     alignItems: 'flex-start',
-    paddingBottom: 5,
+    flex: 2,
   },
-  fundoModal: {
-    flex: 1,
-    backgroundColor: '#00000090',
-    justifyContent: 'center',
+  icone: {
+    padding: 10,
+    height: 50,
+    width: 50,
+    resizeMode: 'stretch',
     alignItems: 'center',
   },
-  modalPrincipal: {
-    height: '30%',
-    width: '70%',
-    backgroundColor: 'white',
-    justifyContent: 'center',
+  section: {
+    flexDirection: 'row',
     alignItems: 'center',
-  },
+    margin: 10,
+    borderWidth: 1,
+    borderRadius: 50,
+    flex: 1
+  }
 });
